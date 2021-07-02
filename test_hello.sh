c8a01271e8922a71538dfa44f04d0b8b833c7dfc
ocabrisses <ocabrisses@cabrisses.net>
2021-07-02 16:43:04 -0400

    Updating with test.

--- test_hello.sh
+++ test_hello.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-[[ `./hello.sh Oscar` = "hello Oscar!" ]] && (echo "test passed!"; exit 0) || (echo "test failed :-("; exit 1)
+[[ `./hello.sh Oscar` = "hello Oscar!" ]] && (echo "test passed!"; exit 0) || (echo "test failed :-("; exit 0)
